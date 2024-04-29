27ececf640abb6b526d37e292b3310e1b094389c
Amelia Cox <Amelia_Cox@corp.ds.gov.nt.ca>
2024-04-29 09:08:28 -0600

    Change buffer size for double burn sites

--- rscript/Identify Road Accessible Lakes with Fire Regime.R
+++ rscript/Identify Road Accessible Lakes with Fire Regime.R
@@ -1,5 +1,6 @@
 library(sf)
 library(tidyverse)
+library(lwgeom)
 
 proj_crs <- 4617
 
@@ -17,7 +18,7 @@ roads2 <- roads %>%
   filter(!RTNUMBER1 %in% c(8,10,"Unknown")| grepl("Kakisa", roads$RTENAME1EN))
 roads_buffer <- roads2 %>% 
   st_union %>% 
-  st_buffer(1000)
+  st_buffer(2000)
 
 # ggplot()+
 #   geom_sf(data=roads, aes(color= RTNUMBER1))+ 
@@ -46,7 +47,7 @@ rm(int, waterbody)
 MasterAccess <- MasterAccess %>% 
   filter(TYPE_TEXT=="Lake")%>% 
   st_zm()
-st_write(MasterAccess, file.path(root, "data", "Road Accessible Lakes_1km.shp"))
+st_write(MasterAccess, file.path(root, "data", "Road Accessible Lakes_2km.shp"))
 #Use Lakemorpho to estimate lake characteristics----
 #Lakemorpho only works for lakes greater than XXX in area
 library(lakemorpho)
@@ -62,7 +63,6 @@ MasterAccess <- MasterAccess %>% mutate(Depth=NA,
                                         AxisRatio=NA, 
                                         Shoreline=NA, 
                                         Volume=NA)
-<<<<<<< HEAD
 #Skip a couple weird shaped lakes (677, 5377, 6087)
 #for(i in c(1:676, 678:nrow(MasterAccess))){
   for(i in c(6088:nrow(MasterAccess))){
@@ -90,6 +90,9 @@ MasterAccess <- MasterAccess %>% mutate(Depth=NA,
 MasterAccess <- st_transform(MasterAccess, proj_crs)
 
 st_write(MasterAccess, file.path(root, "data", "Road Accessible Lakes_1km_add Kakisa.shp"))
+
+MasterAccess <- st_read( file.path(root, "data", "Road Accessible Lakes_1km_add Kakisa.shp"))
+
 
 # Calculate Distance to the Road----
 MasterAccess$DistancetoRoad <-  as.numeric(st_distance(MasterAccess ,
@@ -135,24 +138,41 @@ roadaccessible1k %>%st_drop_geometry()%>% group_by(MostRecentBurn, PreviousBurn)
 
 roadaccessible1k <- roadaccessible1k %>% 
   mutate(FireHistCat= ifelse (is.na(PreviousBurn) & is.na(MostRecentBurn), "No Fire", 
-                                                 ifelse(is.na(PreviousBurn)& MostRecentBurn==2023, "2023 fire", 
-                                                        ifelse(MostRecentBurn %in% c(2012:2015), "~10 years ago", 
-                                                               ifelse(MostRecentBurn==2023 & PreviousBurn %in% c(2012:2015), "2023 and ~10 years ago", NA))))
-)
+                              ifelse(is.na(PreviousBurn)& MostRecentBurn==2023, "2023 fire", 
+                                     ifelse(MostRecentBurn %in% c(2012:2015), "~10 years ago", 
+                                            ifelse(MostRecentBurn==2023 & PreviousBurn %in% c(2012:2015), "2023 and ~10 years ago", NA))))
+  )
+
+roadaccessible1k %>% 
+  st_drop_geometry() %>%
+  group_by(FireHistCat) %>% 
+  summarise(n())
 
 ggplot()+
   geom_sf(data=nwt, fill="white")+
   geom_sf(data=fire %>% 
             filter(fireyear %in% c(2012:2015, 2023)) , 
           aes(fill=factor(fireyear, levels= c(2023, 2012:2015))), 
-          alpha=0.3)+
-  geom_sf(data=roads2)+
+          alpha=0.3, color=NA, show.legend = F)+
+  geom_sf(data=roads2, aes(color= RTENAME1EN), show.legend = F)+
+  geom_sf(data=bounds, color="gray", fill=NA)+
   #geom_sf(data=roads_buffer, fill="yellow", color=NA)+
-  geom_sf(data=roadaccessible1k, aes(fill=FireHistCat), color=NA)+
-  coord_sf(xlim=c(-116, -118), ylim=c(60, 61.5)) #Dehcho + some south slave
-  #coord_sf(xlim=c(-110, -125), ylim=c(60, 63.5)) #entire Map
-ggsave(file.path(root, "figures", "Kakisa accessible lakes Map.png"), units="in",  height=8)
+  geom_sf(data=proposedsites, aes(fill=FireHistCat), color=NA)+
+  geom_sf(data=additional2ksites %>% 
+            filter(FireHistCat %in% c("2023 and ~10 years ago", "~10 years ago")), aes(fill=FireHistCat), color=NA)+
+  coord_sf(xlim=c(-116.75, -117.5), ylim=c(60.86, 61.1))+ #Kakisa area
+  labs(fill=NULL)
+#coord_sf(xlim=c(-116, -118), ylim=c(60.7, 61.3)) #Dehcho + some south slave
+#coord_sf(xlim=c(-110, -125), ylim=c(60, 63.5)) #entire Map
+ggsave(file.path(root, "figures", "Kakisa accessible lakes Map 2.png"), units="in",  height=8, width= 8)
 
+roadaccessible1k %>% st_intersects()
+bounds <- st_bbox(c(xmin=-116.75,xmax=-117.5, ymin=60.88, ymax=61.08), crs= st_crs(proj_crs)) %>% st_as_sfc()
+
+proposedsites <- roadaccessible1k %>% st_intersection(bounds)
+
+proposedsites %>% st_drop_geometry() %>% group_by(FireHistCat) %>% 
+  summarise(n())
 
 ggplot(data=roadaccessible1k %>%st_drop_geometry() %>% filter(!is.na(FireHistCat)), aes(x= DistancetoRoad))+
   geom_histogram()+
@@ -160,3 +180,66 @@ ggplot(data=roadaccessible1k %>%st_drop_geometry() %>% filter(!is.na(FireHistCat
   theme_classic()+
   labs(x= "Distance to nearest road (m)")
 ggsave(file.path(root, "figures", "Distance to road sample options.png"), units="in", width=8, height=8)
+
+
+roadseg <-  stplanr::line_segment (roads2 ,segment_length=10000)
+  roads3 <- roads %>% 
+  filter(!RTNUMBER1 %in% c(8,10,"Unknown")| grepl("Kakisa", roads$RTENAME1EN)) %>% 
+  group_by(RTNUMBER1, RTENAME1EN) %>% 
+    summarize(geometry = st_union(geometry))  %>% 
+    stplanr::line_segment (segment_length=10000)
+  
+  mutate(segID = 1:nrow(roadseg))
+
+
+ggplot()+
+  geom_sf(data=nwt, fill="white")+
+
+  geom_sf(data=roadseg, aes(color=as.factor(segID )), show.legend = F
+          )+
+  coord_sf(xlim=c(-116, -118), ylim=c(60, 61.5))
+
+
+
+###Add additional sites
+
+additional2ksites <- MasterAccess %>% st_intersection(bounds)
+listfire <- st_intersects(additional2ksites, fire2000 )
+
+additional2ksites$MostRecentBurn <-sapply(1:length(listfire), function(x){
+  t<- listfire[[x]]
+  years <- fire2000$fireyear[t]
+  
+  if(length(t)>0){
+    return(years[order(years, decreasing = T)][1])
+  } else {
+    return(NA)
+  }
+  
+})
+
+additional2ksites$PreviousBurn <-sapply(1:length(listfire), function(x){
+  t<- listfire[[x]]
+  years <- fire2000$fireyear[t]
+  if(length(t)>1){
+    return(years[order(years, decreasing = T)][2])
+  } else {
+    return(NA)
+  }
+  
+})
+additional2ksites$DistancetoRoad <-  as.numeric(st_distance(additional2ksites ,
+                                                       roads %>% filter(!RTNUMBER1 %in% c(8,10,"Unknown")| grepl("Kakisa", roads$RTENAME1EN)) %>% st_union))
+
+additional2ksites <- additional2ksites %>% 
+  mutate(FireHistCat= ifelse (is.na(PreviousBurn) & is.na(MostRecentBurn), "No Fire", 
+                              ifelse(is.na(PreviousBurn)& MostRecentBurn==2023, "2023 fire", 
+                                     ifelse(MostRecentBurn %in% c(2012:2015), "~10 years ago", 
+                                            ifelse(MostRecentBurn==2023 & PreviousBurn %in% c(2012:2015), "2023 and ~10 years ago", NA)))), 
+         
+         
+  ) 
+additional2ksites$Area <- st_area(additional2ksites)
+
+View(additional2ksites %>% 
+  filter(FireHistCat =="2023 and ~10 years ago"))
